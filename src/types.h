5025ff5f610e068e5ba7e8771f191ed7577f970b
Shuvo Saha <shuvoshaha641cse@gmail.com>
2026-01-05 21:37:14 +0600

    Update build tools and add uintptr_t type

--- src/types.h
+++ src/types.h
@@ -2,15 +2,16 @@
 #ifndef TYPES_H
 #define TYPES_H
 
-typedef unsigned int   uint32_t;
+typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
-typedef unsigned char  uint8_t;
-typedef int            int32_t;
-typedef short          int16_t;
-typedef char           int8_t;
+typedef unsigned char uint8_t;
+typedef int int32_t;
+typedef short int16_t;
+typedef char int8_t;
 
 typedef uint32_t size_t;
+typedef uint32_t uintptr_t;
 
-#define NULL  ((void*)0)
+#define NULL ((void *)0)
 
 #endif
